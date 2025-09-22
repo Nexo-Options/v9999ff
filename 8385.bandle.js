086de2a779ca8f7592e928296ed5a63483c8f1dc
Nexo Options <nexonexo712@gmail.com>
2025-09-23 01:27:35 +0300

    Create 8385.bandle.js

---
+++ 8385.bandle.js
@@ -0,0 +1,22 @@
+"use strict";(self.webpackChunknexo=self.webpackChunknexo||[]).push([[822,137],{4822:(__unused_webpack_module,__webpack_exports__,__webpack_require__)=>{eval(`
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "default": () => (/* binding */ modules_strategyController),
+  "strategyController": () => (/* binding */ strategyController)
+});
+
+// EXTERNAL MODULES
+var lib_esm = __webpack_require__(95129);
+var logger_lib_esm = __webpack_require__(80711);
+var bignumbers = __webpack_require__(21046);
+var reactivity_esm_bundler = __webpack_require__(2262);
+
+// DEPLOYMENTS
+var arbitrum_USDC = __webpack_require__(39137);
+
+// Example strategy JSON (ETH Straddle v0)
+const HegicStrategyStraddleETH_v0_namespaceObject = JSON.parse('{"Lk":"0x0d22CBaAA16cBb240bc06F1ad6cB4D2cc6AAba81","Mt":[{"inputs":[{"internalType":"contract INexoOperationalTreasury","name":"_pool","type":"address"},{"internalType":"contract AggregatorV3Interface","name":"_priceProvider","type":"address"},{"internalType":"contract IPremiumCalculator","name":"_pricer","type":"address"},{"internalType":"uint8","name":"_spotDecimals","type":"uint8"},{"internalType":"uint256","name":"limit","type":"uint256"}],"stateMutability":"nonpayable","type":"constructor"}, ... ]}');
+`)}}]);
